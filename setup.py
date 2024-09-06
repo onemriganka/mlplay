e8b6037a43b68b1b453accd37a7c81692fc4dae6
Mriganka Barman <mribarman215@gmail.com>
2024-09-06 19:06:31 +0500

    vupdated

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 import codecs
 import os
 
-VERSION = '0.0.7'
+VERSION = '0.0.8'
 DESCRIPTION = 'mlplay'
 LONG_DESCRIPTION = 'radip development of models '
 
